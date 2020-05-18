0a8c4f701786897dc50d3d3a16df7b008d2f9042
moxiaofen <lingruozi321@126.com>
2020-05-18 18:13:17 +0800

    个人信息，头像接口未完成

--- src/page/setting/PersonInfo.vue
+++ src/page/setting/PersonInfo.vue
@@ -10,9 +10,13 @@
     />
 
     <van-cell-group class="content">
-      <van-cell  title="头像">          
-           <img style="width:60px;height:60px" ref='img' alt="handleFile" :src="src" />
-           <input type="file" accept="image/*" class="van-uploader__input hide" ref="inputImg" @change="handleFile">
+      <van-cell  title="头像">  
+        <!-- <form ref="upload" enctype="multipart/form-data" method="post">         
+           <img class="head-img" style="" ref='img' alt="handleFile" :src="src" />
+           <input type="file" accept="image/*" class="van-uploader__input input-file" ref="inputImg" @change="handleFile">
+        </form> -->
+        <img class="head-img" style="" ref='img' alt="handleFile" :src="src" />
+        <input type="file" accept="image/*" class="van-uploader__input input-file" ref="inputImg" @change="handleFile">
       </van-cell>
 
       <van-cell title="用户昵称" is-link :value="loginid"  to="/setting/PersonInfoName" />
@@ -60,29 +64,53 @@
     methods: {
 
       handle() {
-            const file = this.$refs.inputImg.files[0];
-            // createObjectURL传入File类型的数据创建url，可以在浏览器看到网络请求
-            const ObjectURL = URL.createObjectURL(file);
-            const rimg = this.$refs.img;
-            rimg.src = ObjectURL;
-            rimg.onload = function(img) {
-                URL.revokeObjectURL(this.src);  // 释放createObjectURL创建的对象##
-            }
+            // const file = this.$refs.inputImg.files[0];
+            // // createObjectURL传入File类型的数据创建url，可以在浏览器看到网络请求
+            // const ObjectURL = URL.createObjectURL(file);
+            // const rimg = this.$refs.img;
+            // rimg.src = ObjectURL;
+            // rimg.onload = function(img) {
+            //     URL.revokeObjectURL(this.src);  // 释放createObjectURL创建的对象##
+            // }
+            // var file = document.querySelector("#file").files[0];
+            // //创建formdata对象
+            // var formdata = new FormData();
+            // formdata.append("file",file);
             
       },
 
       handleFile(){
            //头像未完成
-           this.handle();
-           const url = this.$api.ROOT + this.$Constants.CHANGE_HEADERIMG;
-           this.$http.post(url,{'userId':sessionStorage.getItem('userId')})
-           .then(function (res) {
-              //  const data = JSON.parse(res.data);
-              //  const records = data.records
-               console.log(res)                           
-          }).catch(function () {
-             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
-          })
+          //  this.handle();
+          //  const url = this.$api.ROOT + this.$Constants.CHANGE_HEADERIMG;
+          //  this.$http.post(url,{'userId':sessionStorage.getItem('userId')})
+          //  .then(function (res) {
+          //     //  const data = JSON.parse(res.data);
+          //     //  const records = data.records
+          //      console.log(res)                           
+          // }).catch(function () {
+          //    this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+          // })
+
+            let config = {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
+            }
+            var fileObj = this.$refs.inputImg.files[0];
+            var formData = new FormData(); 
+            formData.append("file", fileObj);
+            console.log(formData)
+            const url = this.$api.ROOT + this.$Constants.CHANGE_HEADERIMG;
+            this.$http.post(url,formData,config)
+            .then(function (res) {
+                //  const data = JSON.parse(res.data);
+                //  const records = data.records
+                console.log(res)                           
+            }).catch(function () {
+              this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+            })
+
       }
 
 
@@ -103,6 +131,18 @@
 .bar {
   background-color: #FFC600;
   color: #ffffff;
+}
+.head-img{
+  width:60px;
+  height:60px;
+  border-radius: 50%;
+}
+.input-file{
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 100px;
+    opacity: 0;
 }
 
 
